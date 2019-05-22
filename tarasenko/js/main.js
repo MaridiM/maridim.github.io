af420a3adc29e3bd541354d484ff9f218feee91d
MaridiM <maridim92@gmail.com>
2019-05-22 20:31:42 +0300

    Обновление

--- tarasenko/js/main.js
+++ tarasenko/js/main.js
@@ -1,3 +1,18 @@
-let fon = document.querySelectorAll('.fon');
 
-console.log(fon);
+let gallery = document.querySelector('.gallery');
+let img = gallery.querySelectorAll('img');
+
+let el = 0;
+const slider = () => {
+	for (let i = 0; i < img.length; i++) {
+		img[i].style.display = 'none';
+	}
+	el++;
+
+	if(el >= img.length) {
+		el = 0;
+	}
+	img[el].style.display = 'block';	
+}
+
+setInterval(slider, 10000);
