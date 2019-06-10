f04801616df11553d7dd6bac9ec15b69330efee3
MaridiM <maridim92@gmail.com>
2019-06-10 20:40:39 +0300

    Update - add home.js

---
+++ tarasenko/js/home.js
@@ -0,0 +1,17 @@
+let slider = document.querySelector('.slider');
+let img = slider.querySelectorAll('img');
+
+let el = 0;
+const Slider = () => {
+	for (let i = 0; i < img.length; i++) {
+		img[i].style.display = 'none';
+	}
+	el++;
+
+	if(el >= img.length) {
+		el = 0;
+	}
+	img[el].style.display = 'block';	
+}
+
+setInterval(Slider, 5000);
--- tarasenko/js/main.js
+++ tarasenko/js/main.js
@@ -1,21 +1,3 @@
-let slider = document.querySelector('.slider');
-let img = slider.querySelectorAll('img');
-
-let el = 0;
-const Slider = () => {
-	for (let i = 0; i < img.length; i++) {
-		img[i].style.display = 'none';
-	}
-	el++;
-
-	if(el >= img.length) {
-		el = 0;
-	}
-	img[el].style.display = 'block';	
-}
-
-setInterval(Slider, 5000);
-
 // НАЖАТИЕ НА БУРГЕР МЕНЮ
 let burger = document.querySelector('.fas'),
 	nav = document.querySelector('nav'),
