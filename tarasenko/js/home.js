8f5adca81eb8a8e34c2e77fea7b1c5b60a94fba3
MaridiM <maridim92@gmail.com>
2019-06-14 01:51:49 +0300

    Update
    Update  and optimisation  slider in  home  page

--- tarasenko/js/home.js
+++ tarasenko/js/home.js
@@ -1,9 +1,12 @@
 let slider = document.querySelector('.slider');
+let desktop = document.querySelector('#desktop');
+let mobile = document.querySelector('#mobile');
 let img = slider.querySelectorAll('img');
 
 let el = 0;
 const Slider = () => {
 	for (let i = 0; i < img.length; i++) {
+		mobile.style.display = 'none';
 		img[i].style.display = 'none';
 	}
 	el++;
@@ -11,7 +14,9 @@ const Slider = () => {
 	if(el >= img.length) {
 		el = 0;
 	}
-	img[el].style.display = 'block';	
+	img[el].style.display = 'block';
+
+	
 }
 
 setInterval(Slider, 5000);
